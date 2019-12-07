a9a7312ca071656429e5ab89f03b5cae45f60d9e
ChristianGeng <christian.c.geng@gmail.com>
2019-12-07 14:22:37 +0100

    add python taks

--- src/data/wav2features.sh
+++ src/data/wav2features.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
 echo "Hello Wrokd!"
-
 indir="../../data/raw/wav/"
 conffile="/home/christian/bin/opensmile-2.3.0/config/IS13_ComParE.conf"
-outfile="../../data/processed/data.csv"
+outfile_features="../../data/processed/features.csv"
+outfile_labels="../../data/processed/filenames.csv"
 
 echo $indir
 echo $conffile
-echo $outfile
+echo $outfile_features
 
 # TODO: using env variables from .env or so
 # SMILExtract="~/bin/opensmile-2.3.0/bin/linux_x64_standalone_static/SMILExtract"
-# rm -fv ${outdir}"*.csv"
+rm -f ${outfile_features}
+echo "filename" > ${outfile_labels}
 
 for fname in ${indir}*.wav; do
     if [ -f ${fname} ]; then
         echo "processing "${fname}
-        SMILExtract -C ${conffile} -I ${fname} -start 0 -end -1  -csvoutput ${outfile} -appendcsv 1
+        SMILExtract -C ${conffile} -I ${fname} -start 0 -end -1  -csvoutput ${outfile_features} -appendcsv 1
+        echo $fname
+        filename=$(basename -- "$fname")
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+        echo ${filename}
+        # emotion=$(echo ${filename} | cut  -c 6)
+        echo ${filename} >> ${outfile_labels}
     else
         echo "File not found!"
     fi
